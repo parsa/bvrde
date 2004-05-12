e5740e7856580cc82d8e54eb38915afae3b1d82e
bviksoe <>
2004-05-12 19:43:29 +0000

    *** empty log message ***

---
+++ clean.cmd
@@ -0,0 +1,15 @@
+del /S *.pch
+del /S *.sbr
+del /S *.res
+del /S *.idb
+del /S *.ncb
+del /S *.opt
+del /S *.obj
+del /S *.ilk
+del /S *.pdb
+del /S *.plg
+del /S *.aps
+del /S *.bsc
+del /S *.exp
+del /S *.tmp
+
